fe9bec284ca3273494a2a240a40b6bceb6bd87a4
KyleLoOG <kyle.r.wilcocks@hotmail.com>
2014-10-20 16:14:50 -0400

    Added DrawSquarefunction
    adsasdasd

--- Midterm Template/Midterm/SFMLOpenGLTemplate/main.cpp
+++ Midterm Template/Midterm/SFMLOpenGLTemplate/main.cpp
@@ -5,10 +5,13 @@
 void init();
 void DrawGame();
 void DrawUI();
-void DrawSquare();
+void DrawSquare(float width,float height);
 void DrawTriangle();
 void Viewports();
 void KeyboardHandle();
+
+float xtranslate[];
+float ytranslate[];
 
 GLuint windowWidth = 1024;
 GLuint windowHeight = 768;
@@ -55,12 +58,13 @@ void init()
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(90.f, 1.f, 1.f, 500.f); 	
-	glEnable(GL_TEXTURE_2D);							
+
+	/*glEnable(GL_TEXTURE_2D);							
 	img_data.loadFromFile("images/box.bmp");
 	glGenTextures(1, &texture_handle);	
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img_data.getSize().x, img_data.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data.getPixelsPtr());
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);*/
 	
 
 }
@@ -70,17 +74,22 @@ void Viewports(){
 	glViewport(0, 0, windowWidth, windowHeight);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(90.f, 1.f, 1.f, 1000.f);
-
-
-
-
-
+	glOrtho(-20, 20, -20, 20, 0, 1000);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	gluLookAt(0, 0, -10, 0, 0, 0, 0, 1, 0);
+	
+	DrawGame();
 }
 
 void DrawGame(){
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+
+	DrawSquare(5, 9);
+	
+
 }
 
 void DrawUI(){
@@ -112,14 +121,14 @@ void KeyboardHandle(){
 
 }
 
-void DrawSquare(){
-
+void DrawSquare(float width,float height){
+	
 	glBegin(GL_QUADS);
 	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(0.0f, 0.0f);
-	glVertex2f(1.0f, 1.0f);
-	glVertex2f(0.0f, 0.0f);
-	glVertex2f(0.0f, 0.0f);
+	glVertex2f(width, height);
+	glVertex2f(-width, height);
+	glVertex2f(-width,-height);
+	glVertex2f(width,-height);
 	glEnd();
 
 }
